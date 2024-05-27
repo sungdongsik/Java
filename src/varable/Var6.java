af07779378904da2410e005c0ad171770599b945
성동식 <tjdehdtlr123@naver.com>
2024-05-28 00:55:03 +0900

    자바 기초 문법

---
+++ src/varable/Var6.java
@@ -0,0 +1,9 @@
+package varable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a; //초기화하면 실행이 안됨
+        //System.out.println("a = " + a);
+    }
+}
---
+++ src/varable/Var7.java
@@ -0,0 +1,17 @@
+package varable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100;//정수
+        double b = 10.5; //실수
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello Java";
+
+        System.out.println("a = " + a);
+        System.out.println("a = " + b);
+        System.out.println("a = " + c);
+        System.out.println("a = " + d);
+        System.out.println("a = " + e);
+    }
+}
